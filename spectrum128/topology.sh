3ef1962aab8e356dfe2567a36d9c33ba6f473df3
Pete Moore <pmoore@mozilla.com>
2020-09-20 16:22:44 +0200

    wip

---
+++ spectrum128/topology.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "${0}")"
+
+for ROM in 0 1; do
+  cat "../zxspectrum_roms/Spectrum128K_ROM${ROM}.asm" | sed 's/;.*//' | sed '/^ *$/d' > "rom${ROM}"
+  cat "rom${ROM}" | grep 'JP' | sort -u
+  cat "rom${ROM}" | grep 'JR' | sort -u
+  cat "rom${ROM}" | grep 'CALL' | sort -u
+  cat "rom${ROM}" | grep 'DJNZ' | sort -u
+  cat "rom${ROM}" | grep 'RET' | sort -u
+  rm "rom${ROM}"
+done
