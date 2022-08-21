878b105097a836a9369c77dfc8960ffe1415d0aa
Pete Moore <pmoore@mozilla.com>
2022-08-21 11:51:43 +0200

    fix

--- screendump/convert/main.go
+++ screendump/convert/main.go
@@ -45,18 +45,25 @@ func main() {
 		log.Fatal(err)
 	}
 	count := 0
-	oldj := uint64(0)
-	for i := 0; i < 213840; i += 8 {
-		j := uint64(data[i]) + uint64(data[i+1])<<8 + uint64(data[i+2])<<16 + uint64(data[i+3])<<24 + uint64(data[i+4])<<32 + uint64(data[i+5])<<40 + uint64(data[i+6])<<48 + uint64(data[i+7])<<56
-		if i == 0 || j != oldj {
+	j := uint64(data[0]) + uint64(data[1])<<8 + uint64(data[2])<<16 + uint64(data[3])<<24 + uint64(data[4])<<32 + uint64(data[5])<<40 + uint64(data[6])<<48 + uint64(data[7])<<56
+	var oldj uint64
+	for i := 8; i < 213840; i += 8 {
+        oldj = j
+		j = uint64(data[i]) + uint64(data[i+1])<<8 + uint64(data[i+2])<<16 + uint64(data[i+3])<<24 + uint64(data[i+4])<<32 + uint64(data[i+5])<<40 + uint64(data[i+6])<<48 + uint64(data[i+7])<<56
+		if j != oldj {
 			if count == 0 {
-				fmt.Printf("0x%016x ", j)
+				fmt.Printf("0x%016x ", oldj)
 			} else {
-				fmt.Printf("0x6a09e667bb67ae85 0x%016x 0x%016x ", count, j)
+				fmt.Printf("0x6a09e667bb67ae85 0x%016x 0x%016x ", count, oldj)
 			}
 			count = 0
 		} else {
 			count += 1
 		}
+	}
+	if count == 0 {
+		fmt.Printf("0x%016x ", j)
+	} else {
+		fmt.Printf("0x6a09e667bb67ae85 0x%016x 0x%016x ", count, j)
 	}
 }
