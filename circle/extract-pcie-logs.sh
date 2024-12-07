ef6ea5567afdf6978fed0a2d0488014415a6c07e
Pete Moore <pmoore@mozilla.com>
2024-12-07 15:54:00 +0100

    wip

--- circle/extract-pcie-logs.sh
+++ circle/extract-pcie-logs.sh
@@ -11,6 +11,6 @@ cat circle.log | grep pcie | sed 's/\r//g' | sed 's/\[/[0x/' | sed 's/ = /=0x000
    	| sed -E 's/(.*write)8(.*)=0x0*(.{2})$/\1\2=0x\3/' \
 	| sed 's/.*read/read/' | sed 's/.*write/write/' > circle_pcie.log
 
-cat spectrum4.log | sed -n -E '/^(read|write)/p' | sed 's/\[0x00000000/[0x/' > spectrum4_pcie.log
+cat spectrum4.log | sed 's/\r//g' | sed 's/\[0xffff/[0x0000/' | sed -n -E '/^(read|write)/p' | sed 's/\[0x00000000/[0x/' > spectrum4_pcie.log
 
 vim -d spectrum4_pcie.log circle_pcie.log
