38c9261549ef209469fdded043837074bfce6870
Pete Moore <pmoore@mozilla.com>
2025-09-28 12:36:39 +0200

    wip

--- circle/extract-pcie-logs.sh
+++ circle/extract-pcie-logs.sh
@@ -5,7 +5,7 @@
 
 cd "$(dirname "${0}")"
 
-cat circle.log | grep pcie | sed 's/\r//g' | sed 's/\[/[0x/' | sed 's/ = /=0x00000000000000000000/' \
+cat circle.log | grep '\(read\|write\)' | sed 's/\r//g' | sed 's/\[/[0x/' | sed 's/ = /=0x00000000000000000000/' \
    	| sed -E 's/(.*read)32(.*)=0x0*(.{8})$/\1\2=0x\3/' \
    	| sed -E 's/(.*write)32(.*)=0x0*(.{8})$/\1\2=0x\3/' \
    	| sed -E 's/(.*read)16(.*)=0x0*(.{4})$/\1\2=0x\3/' \
@@ -14,6 +14,6 @@ cat circle.log | grep pcie | sed 's/\r//g' | sed 's/\[/[0x/' | sed 's/ = /=0x000
    	| sed -E 's/(.*write)8(.*)=0x0*(.{2})$/\1\2=0x\3/' \
 	| sed 's/.*read/read/' | sed 's/.*write/write/' > circle_pcie.log
 
-cat spectrum4.log | sed 's/\r//g' | sed 's/\[0xffff/[0x0000/' | sed -n -E '/^(read|write)/p' | sed 's/\[0x00000000/[0x/' > spectrum4_pcie.log
+cat spectrum4.log | sed 's/\r//g' | sed 's/\[0xfffffff/[0x0000000/' | sed -n -E '/^(read|write)/p' | sed 's/\[0x00000000/[0x/' > spectrum4_pcie.log
 
 vim -d spectrum4_pcie.log circle_pcie.log
