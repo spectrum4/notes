ce9f3718b58138802f8680e0c0419279249018d8
Pete Moore <pmoore@mozilla.com>
2025-10-19 14:33:28 +0200

    wip

--- circle/extract-pcie-logs.sh
+++ circle/extract-pcie-logs.sh
@@ -18,5 +18,29 @@ cat ../screenlog.0 | sed 's/\r//g' | sed 's/\[0xfffffff/[0x0000000/' | sed -n -E
 cat circle_pcie.log  | grep -vF '[0xfd5' | grep -vF '[0xfe' | more | sed 's/\[0x\(...\]\)/[0x600000\1/g' | sed 's/\[0x\(..\]\)/[0x6000000\1/g' | sed 's/\[0x\(.\]\)/[0x60000000\1/g' > circle_xhci.log
 cat spectrum4_pcie.log | sed 's/\[0x00000006/[0x6/g' | grep '\[0x60000....\]' > spectrum4_xhci.log
 
-vim -d spectrum4_pcie.log circle_pcie.log
+for file in circle_xhci.log spectrum4_xhci.log; do
+  cat "${file}" > x
+  cat x \
+    | sed 's/.*\[0x600000002\].*/& XHCI_REG_CAP_HCIVERSION/' \
+    | sed 's/.*\[0x600000020\].*/& USBCMD/' \
+    | sed 's/.*\[0x600000024\].*/& USBSTS/' \
+	| sed 's/.*\[0x600000038\].*/& CRCR (lo)/' \
+	| sed 's/.*\[0x60000003c\].*/& CRCR (hi)/' \
+    | sed 's/.*\[0x600000050\].*/& XHCI_REG_OP_DCBAAP_LO/' \
+    | sed 's/.*\[0x600000054\].*/& XHCI_REG_OP_DCBAAP_HI/' \
+    | sed 's/.*\[0x600000058\].*/& XHCI_REG_OP_CONFIG/' \
+    | sed 's/.*\[0x600000100\].*/& host controller doorbell/' \
+    | sed 's/.*\[0x600000220\].*/& IMAN/' \
+    | sed 's/.*\[0x600000224\].*/& IMOD/' \
+    | sed 's/.*\[0x600000228\].*/& ERSTSZ/' \
+	| sed 's/.*\[0x600000230\].*/& ERSTBA (lo)/' \
+	| sed 's/.*\[0x600000234\].*/& ERSTBA (hi)/' \
+	| sed 's/.*\[0x600000238\].*/& ERDP (lo)/' \
+	| sed 's/.*\[0x60000023c\].*/& ERDP (hi)/' \
+	| sed 's/.*\[0x600000420\].*/& PORTSC (port 1)/' \
+      > "${file}"
+  rm x
+done
+
 vim -d spectrum4_xhci.log circle_xhci.log
+vim -d spectrum4_pcie.log circle_pcie.log
